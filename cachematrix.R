c6ce5d039dcf438c71ff93d5667e2ef272a85123
fernfant <ffanton@gmail.com>
2015-02-05 22:25:42 +0000

    Adding comments
    Adding comments to both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a matrix and returns a list inlcuding accessors such as setter and getter
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
+                ## the setter functions assings variables in a different environment by ussing << operator
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
         setmatrix <- function(matrix) m <<- matrix
         getmatrix <- function() m
+        
+        ## list to be returned by the function, which includes accessors for the matrix
         list(set = set, get = get,
              setmatrix = setmatrix,
              getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the matrix X, while previously checking is not present on the cache already
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getmatrix()
+        
+        ## Check whether inverse is already presenrt in the cache, if so returns cache version
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        
+        ## If not present in the cache, then calculates the inverse using solve
         data <- x$get()
         m <- solve(data, ...)
         x$setmatrix(m)
